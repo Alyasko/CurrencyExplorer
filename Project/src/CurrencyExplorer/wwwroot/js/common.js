f2c7ec7c1dfa75d964c68917a8ed6827c256782f
Alexander Yasko <yasko.alexander@gmail.com>
2016-02-24 21:25:30 +0200

    Added menu behavior

---
+++ Project/src/CurrencyExplorer/wwwroot/js/common.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    // set handlers for explorer nav buttons
+
+
+    var $menu = $("#main-menu");
+
+    var offset = $menu.offset().top;
+    var isFixed = true;
+
+    var $wind = $(window);
+    $wind.scroll(function () {
+        if ($wind.scrollTop() > offset && isFixed) {
+            $menu.addClass("main-menu-floating");
+            isFixed = false;
+        }
+        else if ($wind.scrollTop() < offset && !isFixed) {
+            $menu.removeClass("main-menu-floating");
+            isFixed = true;
+        }
+    });
+});
