6d1fe4f6b592878794139768811494ecd38f0855
Alexander <yasko.alexander@gmail.com>
2016-03-10 22:48:08 +0200

    Working project.

---
+++ Project/src/CurrencyExplorer/Models/ApiDatabaseCachingProcessor.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CurrencyExplorer.Models.Contracts;
+using CurrencyExplorer.Models.Entities;
+
+namespace CurrencyExplorer.Models
+{
+    public class ApiDatabaseCachingProcessor : ICachingProcessor
+    {
+        private ICurrencyProvider _iCurrencyProvider;
+
+        public ApiDatabaseCachingProcessor()
+        {
+            _iCurrencyProvider = null;
+            Data = null;
+        }
+
+        public IDictionary<CurrencyCode, CurrencyData> RequestSingleData(DateTime timePeriod, IEnumerable<CurrencyCode> codes)
+        {
+            bool existsInDb = CheckDbData();
+            IDictionary<CurrencyCode, CurrencyData> singleCurrencyData =
+                new Dictionary<CurrencyCode, CurrencyData>();
+
+
+            if (existsInDb)
+            {
+                // Select data from DB.
+            }
+            else
+            {
+                // Download data from API.
+
+                foreach (CurrencyCode currencyCode in codes)
+                {
+                    _iCurrencyProvider.RequestCurrencyData(timePeriod, currencyCode);
+
+                    CurrencyData data = _iCurrencyProvider.Data;
+
+                    singleCurrencyData.Add(currencyCode, data);
+                }
+            }
+
+            return singleCurrencyData;
+        }
+
+        public IDictionary<CurrencyCode, IEnumerable<CurrencyData>> RequestPeriodData(ChartTimePeriod timePeriod, IEnumerable<CurrencyCode> codes)
+        {
+            IDictionary<CurrencyCode, List<CurrencyData>> periodCurrencyData =
+                new Dictionary<CurrencyCode, List<CurrencyData>>();
+
+            DateTime beginTime = timePeriod.Begin;
+            DateTime endTime = timePeriod.End;
+
+            foreach (CurrencyCode code in codes)
+            {
+                periodCurrencyData.Add(code, new List<CurrencyData>());
+            }
+
+            // TODO: fix iterator to make possible to change the step of selection so that select less data in large queries.
+            for (DateTime iterator = beginTime; iterator < endTime; iterator = iterator.AddDays(1))
+            {
+                IDictionary<CurrencyCode, CurrencyData> currentSingleData = this.RequestSingleData(iterator, codes);
+
+                foreach (var pair in currentSingleData)
+                {
+                    periodCurrencyData[pair.Key].Add(pair.Value);
+                }
+            }
+
+            // TODO: fix IEnumerable
+            return periodCurrencyData as IDictionary<CurrencyCode, IEnumerable<CurrencyData>>;
+        }
+
+
+        private bool CheckDbData()
+        {
+            return false;
+        }
+
+        public IEnumerable<IDictionary<CurrencyCode, ChartCurrencyDataPoint>> Data { get; private set; }
+    }
+}
--- Project/src/CurrencyExplorer/Startup.cs
+++ Project/src/CurrencyExplorer/Startup.cs
@@ -2,26 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CurrencyExplorer.Models;
+using CurrencyExplorer.Models.Contracts;
 using Microsoft.AspNet.Antiforgery;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.OptionsModel;
+using Microsoft.Extensions.PlatformAbstractions;
 
 namespace CurrencyExplorer
 {
     public class Startup
     {
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
             services.AddMvc();
+
+            services.AddScoped<CurrencyXplorer>();
+            services.AddScoped<ICachingProcessor, ApiDatabaseCachingProcessor>();
+            services.AddScoped<ICurrencyProvider, NationalBankCurrencyProvider>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
+            //var appSettings = app.ApplicationServices.GetService<IOptions<AppSettings>>();
+            //string s = appSettings.Value.SiteTitle;
+
             app.UseStaticFiles();
             app.UseMvc(config =>
             {
